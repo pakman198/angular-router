f0de33566714df65aa27dd630793ec3046572fe0
pako herrera <pakman_198@hotmail.com>
2019-06-23 23:36:07 -0700

    Milestone 5. Added admin module with several components and routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ComposeMessageComponent } from './compose-message/compose-message.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesModule } from './heroes/heroes.module';
 import { CrisisCenterModule } from './crisis-center/crisis-center.module';
-import { ComposeMessageComponent } from './compose-message/compose-message.component';
+
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,7 @@ import { ComposeMessageComponent } from './compose-message/compose-message.compo
     FormsModule,
     HeroesModule,
     CrisisCenterModule,
+    AdminModule,
     AppRoutingModule,
   ],
   providers: [],
