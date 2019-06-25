90a951ce59c54f6366b37dfcc9982507e216c261
pako herrera <pakman_198@hotmail.com>
2019-06-24 19:24:09 -0700

    Milestone 6. Migrating URLs with redirects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesModule } from './heroes/heroes.module';
 import { AuthModule } from './auth/auth.module';
+import { Router } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -28,4 +29,12 @@ import { AuthModule } from './auth/auth.module';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  // Diagnostic only: inspect router configuration
+  constructor(router: Router) {
+    // Use a custom replacer to display function names in the route configs
+    const replacer = (key, value) => (typeof value === 'function') ? value.name : value;
+
+    console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+  }
+}
--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -4,13 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeroListComponent }    from './hero-list/hero-list.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
-const routes: Routes = [
-  {path: 'superheroes', component: HeroListComponent, data: { animation: 'heroes' }},
-  {path: 'hero/:id', component: HeroDetailComponent, data: { animation: 'hero' }}
+const heroesRoutes: Routes = [
+  { path: 'heroes', redirectTo: '/superheroes' },
+  { path: 'hero/:id', redirectTo: '/superhero/:id' },
+  { path: 'superheroes',  component: HeroListComponent, data: { animation: 'heroes' } },
+  { path: 'superhero/:id', component: HeroDetailComponent, data: { animation: 'hero' } }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(heroesRoutes)],
   exports: [RouterModule]
 })
 export class HeroesRoutingModule { }
